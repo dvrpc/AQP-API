e371464194f261ae0493f75dcd52a97f408a49ac
Kris Warner <kris@kdwarn.net>
2025-05-16 17:09:19 -0400

    Add db_port env var

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const db = new pg.Pool({
   host: process.env.db_host || "127.0.0.1",
   database: process.env.db_name || "aqp",
   password: process.env.db_user_pass || "postgres",
-  port: 5432,
+  port: process.env.db_port || "5432",
 });
 
 export default db;
