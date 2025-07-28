77d8f5fc7ccd1adcb40f1d6b3794aa4a6b6dff66
hachadorian <hachadorian98@gmail.com>
2025-07-28 16:50:12 -0400

    formatting, empty aqi check

--- populate.js
+++ populate.js
@@ -4,15 +4,17 @@ import db from "./db";
 import "./dateFormat";
 
 const filterByDate = (date, results) => {
-  const todayColl = results.filter(result => result[1] === date && result[6] === "Y");
+  const todayColl = results.filter(
+    (result) => result[1] === date && result[6] === "Y",
+  );
   if (!todayColl.length) {
     return null;
   }
   if (todayColl.length === 1) {
-    return todayColl[0]
+    return todayColl[0];
   }
-  return todayColl.sort((a,b) => (a[5] < b[5] ? 1 : -1))[0];
-}
+  return todayColl.sort((a, b) => (a[5] < b[5] ? 1 : -1))[0];
+};
 
 const populate = () => {
   const date = new Date().addHours(-5);
@@ -20,34 +22,31 @@ const populate = () => {
   const dates = [
     date.addDays(-1).customFormat(),
     date.customFormat(),
-    date.addDays(1).customFormat()
+    date.addDays(1).customFormat(),
   ];
   https.get(
     "https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/today/reportingarea.dat",
-    res =>
+    (res) =>
       res
         .pipe(csv({ headers: false, separator: "|" }))
-        .on("data", data => data[7] === "Philadelphia" && results.push(data))
+        .on("data", (data) => data[7] === "Philadelphia" && results.push(data))
         .on("end", async () => {
           await db.query(
-            "CREATE TABLE IF NOT EXISTS forecasts (date TEXT PRIMARY KEY, aqi INTEGER, forecast TEXT, updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+            "CREATE TABLE IF NOT EXISTS forecasts (date TEXT PRIMARY KEY, aqi INTEGER, forecast TEXT, updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP)",
           );
           dates
-            .map(date => {
+            .map((date) => {
               const current = filterByDate(date, results);
-              return [
-                date,
-                current[12],
-                current[15]
-              ];
+              const aqi = parseInt(current[12]) ? current[12] : "0";
+              return [date, aqi, current[15]];
             })
-            .forEach(vals =>
+            .forEach((vals) =>
               db.query(
                 "INSERT INTO forecasts (date, aqi, forecast) VALUES ($1,$2,$3) ON CONFLICT (date) DO UPDATE SET aqi = $2, forecast = $3, updated = CURRENT_TIMESTAMP",
-                vals
-              )
+                vals,
+              ),
             );
-        })
+        }),
   );
 };
 export default populate;
